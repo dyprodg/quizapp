c975da812180f8ee3f7e3814544cb1534690c348
dyprodg <dennisdiepolder@outlook.com>
2023-09-27 14:30:59 +0200

    b

--- index.js
+++ index.js
@@ -7,47 +7,59 @@ const path = require('path');
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// EJS View Engine Setup
+// EJS Setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));  // Setzen des 'views'-Pfads
 
-let currentQuestionIndex = 0;
+
 let score = 0;
 
+//homepage
 app.get('/', (req, res) => {
     res.render('index', { quizStarted: false });
 });
 
+//start quiz button
 app.get('/startQuiz', (req, res) => {
-    currentQuestionIndex = 0;
+    questionsAsked = 0;
     score = 0;
     res.redirect('/nextQuestion');
 });
 
+//Quiz oder Ergebnis
 app.get('/nextQuestion', (req, res) => {
-    if (currentQuestionIndex < quiz.length) {
+        
+        if (questionsAsked < 10) {
+            indexQuestion = Math.floor(Math.random() * 101),
         res.render('index', {
+            
             quizStarted: true,
-            question: quiz[currentQuestionIndex],
+            question: quiz[indexQuestion],
             score: score
         });
-    } else {
-        // Ändern der Ausgabe, wenn das Quiz vorbei ist
-        res.render('quizEnd', {
-            score: score
-        });
+    } else { 
+        if(score < 7) {
+            res.render('dumb', {
+                score: score
+            });
+        } else {
+            res.render('quizEnd', {
+                score: score
+            });
+        }
+        
     }
 });
 
+//Quiz frage pruefen
 app.post('/submitAnswer', (req, res) => {
     const userAnswerIndex = parseInt(req.body.answer);
-    const correctAnswer = quiz[currentQuestionIndex].antworten.find(a => a.istRichtig);
     
-    if (quiz[currentQuestionIndex].antworten[userAnswerIndex].istRichtig) {
+    if (quiz[indexQuestion].antworten[userAnswerIndex].istRichtig) {
         score++;
     }
 
-    currentQuestionIndex++;
+    questionsAsked++;
     res.redirect('/nextQuestion');
 });
 
@@ -55,3 +67,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server läuft auf http://localhost:${PORT}`);
 });
+
+
